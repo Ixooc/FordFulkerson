b365b4edc6863fe612771414026843dfa3b0c8bc
Fabrizio Alonso Santi Guerrero <u202411774@upc.edu.pe>
2025-11-05 23:40:01 -0500

    Finalizado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 from gui import FordFulkersonGUI
+import matplotlib.pyplot as plt  # <-- 1. Importa matplotlib.pyplot
 
 if __name__ == "__main__":
     ctk.set_appearance_mode("Dark")  
@@ -7,4 +8,15 @@ if __name__ == "__main__":
 
     root = ctk.CTk() 
     app = FordFulkersonGUI(root)
+
+    def _on_closing():
+        try:
+            plt.close(app.fig)
+        except Exception as e:
+            print(f"Error al cerrar Matplotlib: {e}") 
+            
+        root.destroy()
+    
+    root.protocol("WM_DELETE_WINDOW", _on_closing)
+
     root.mainloop()
